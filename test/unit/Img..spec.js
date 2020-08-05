0dda765149ea1498d09f8e1d878ed572e88bdd21
houtianfu <houtianfu@jd.com>
2020-08-05 22:22:10 +0800

    test(img): finished

---
+++ test/unit/Img..spec.js
@@ -0,0 +1,19 @@
+import Img from 'wot-design/packages/img/index.js'
+import { mount } from '@vue/test-utils'
+import { testInstall } from './utils'
+
+testInstall(Img)
+
+test('图片应该正确的渲染', () => {
+  let wrapper = mount(Img)
+  expect(wrapper.contains('.wd-img')).toBe(true)
+})
+
+test('应该渲染为圆形图片', () => {
+  let wrapper = mount(Img, {
+    propsData: {
+      round: true
+    }
+  })
+  expect(wrapper.contains('.round')).toBe(true)
+})
